cd6dd0ba10ecfc6b2c0a0fb9e231598c3b5a237b
kyleonaut <kyleonaut@gmail.com>
2021-11-01 16:46:20 +0100

    fix: typo + nullpointer

--- src/main/java/de/kyleonaut/serverstatus/ServerStatusPlugin.java
+++ src/main/java/de/kyleonaut/serverstatus/ServerStatusPlugin.java
@@ -1,6 +1,7 @@
 package de.kyleonaut.serverstatus;
 
 import lombok.SneakyThrows;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
@@ -18,20 +19,27 @@ public class ServerStatusPlugin extends JavaPlugin {
         saveDefaultConfig();
         this.url = getConfig().getString("url");
         this.server = getConfig().getString("server");
+        if (this.url == null){
+            System.out.println("[ServerStatus] §cYou have to provide a webhook url!");
+            return;
+        }
         final DiscordWebhook discordWebhook = new DiscordWebhook(this.url);
         discordWebhook.setUsername("ServerStatus");
-        discordWebhook.setAvatarUrl("https://pic.onlinewebfonts.com/svg/img_569193.png");
-        discordWebhook.setContent("Server " + this.server + "started :green_circle:");
+        discordWebhook.setAvatarUrl("https://static-cdn.jtvnw.net/jtv_user_pictures/fb81cae2-1aa2-44ba-a818-ecaa535ba9bd-profile_image-150x150.png");
+        discordWebhook.setContent("Server " + this.server + " started successfully :green_circle:");
         discordWebhook.execute();
     }
 
     @SneakyThrows
     @Override
     public void onDisable() {
+        if (this.url == null){
+            return;
+        }
         final DiscordWebhook discordWebhook = new DiscordWebhook(this.url);
         discordWebhook.setUsername("ServerStatus");
         discordWebhook.setAvatarUrl("https://pic.onlinewebfonts.com/svg/img_569193.png");
-        discordWebhook.setContent("Server " + this.server + "stopped :red_circle:");
+        discordWebhook.setContent("Server " + this.server + " stopped :red_circle:");
         discordWebhook.execute();
     }
 }
